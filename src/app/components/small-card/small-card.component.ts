31a5d30219d5fb0f07b2f6b45d14924e52fce7e4
Roberto Araripe <roberto.araripe@outlook.com>
2024-01-19 16:43:59 -0300

    Ajustes de dinamismo dos componentes.

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SmallCardComponent implements OnInit {
 
+  @Input()
+  photoCover:string="https://www.crunchyroll.com/imgsrv/display/thumbnail/1200x675/catalog/crunchyroll/21907feaa299ecdc23d6e64e59b45002.jpe"
+  @Input()
+  cardTitle:string="Shin Godzilla causou polêmica com seus poderes, poderá ter sido vencido por Minus One?"
   constructor() { }
 
   ngOnInit(): void {
